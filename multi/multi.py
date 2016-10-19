6eaa3ec75b927efecca65a6e100513bd423f4e45
Stephen Swaney <stephen.d.swaney@gmail.com>
2016-10-19 02:07:04 -0400

    add simpler command interpreter to MotorController class
    Language is:
    step N
    quit
    other messages are ignored
    
    tell .gitignore to ignore emacs tmp files

--- multi/multi.py
+++ multi/multi.py
@@ -3,67 +3,110 @@
 from multiprocessing import Process, Queue
 import os
 import time
+import shlex
+
+
+# stepper sequence.  motor dependent
+SEQ = [(1,0,0,0),
+       (0,1,0,0),
+       (1,1,0,0),
+       (0,0,1,0),
+       (1,0,1,0),
+       (0,1,1,0),
+       (1,1,1,0),
+       (1,1,1,1)]
+
+PINS = [13,15,16,18]
 
 
 class MotorController:
-    """R-Pi stepper controller."""
+    """R-Pi stepper controller.
+    language:
+    step N  - do N rotation steps
+    quit    - kill subprocess
+    """
+
     def __init__(self, name, msgque):
         self.name  = name
         self.que = msgque
+        self.seq = SEQ
+        self.next = 0
+        self.pins = PINS
         self.proc = Process(target=self.run, args=())
         self.proc.start()
 
-
     def run(self):
-        msg = self.que.get()
-        print('msg:', msg)
-        msg = self.que.get()
-        print('msg:', msg)
-        
-    
+        runit = True
+        while(runit):
+            msg = shlex.split(self.que.get())
+            if msg:
+                print('msg:', msg)
+                if msg[0] == 'quit':
+                    break
+                if msg[0] == 'step':
+                    # step() wants a number, not a string
+                    self.step( int(msg[1]) )
+
     def step(self, steps):
         """cycle motor steps number of steps"""
-        print("step %d\n")
-        pass
+        print("step %d" % steps)
+        # for each step, set the pins for the current pattern
+        for s in range(steps):
+            self.toggle_pins(self.seq[self.next],
+                             self.pins)
+            # cycle next ptr thru sequence
+            self.next += 1
+            if self.next >= len(self.seq):
+                self.next = 0
+
+    def toggle_pins( self,seq, pins):
+        print(seq)
+        # todo
+        # set pins from sequence
         
-    def read(self):
-        x = self.que.get()
-        print(x)
-    
 
 def info(title):
+    """ display some process info """
     print(title)
     #print('module name:', __name__)
     #print('parent process:', os.getppid())
     print('process id:', os.getpid())
     print
-    
 
 
-    
-def f(name):
-    #info('function f')
-    print('hello',   name)
-    
-    
+#
+# main line
+#
 
 if __name__ == '__main__':
+    # process id
     print('\t= my pid:%d '% os.getpid())
     print('\t======')
-    names = ['stepX', 'stepY', 'stepZ']
-    controls = []
+
+    # names from controllers
+    # names = ['stepX', 'stepY', 'stepZ']
+    names = ['stepX']
+
+    # list of controllers
+    controls = []   
+    # create a controller & its que.  add to our list of controls
     for nam in names:
             que = Queue()
             mc = MotorController( nam, que)
             controls.append(mc)
 
+    # run some commands
+    for con in controls:
+        con.que.put( con.name) # send controller name. a NOOP cmd
+    time.sleep(1)
 
-    # run
     for con in controls:
-        con.que.put( con.name)
-    time.sleep(2)
+        con.que.put( 'step 10')  # do some steps
+    time.sleep(1)
+
+    # stop controller sub process
     for con in controls:
-        con.que.put( con.name + '2')
+        con.que.put('quit')
 
     
     # time to quit
