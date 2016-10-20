0d15645ef245c626d19df022a1e5a1c1e50d8e06
Stephen Swaney <stephen.d.swaney@gmail.com>
2016-10-20 12:21:35 -0400

    Factor out stepper controller class into stepper.py
    add GPIO functions

---
+++ main.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+from multiprocessing import Process, Queue
+import os
+import time
+import shlex
+import RPi.GPIO as GPIO
+from stepper_controller import MotorController
+
+
+# stepper sequence.  motor dependent
+# SEQ = [(1,0,0,0),
+#        (0,1,0,0),
+#        (1,1,0,0),
+#        (0,0,1,0),
+#        (1,0,1,0),
+#        (0,1,1,0),
+#        (1,1,1,0),
+#        (0,0,0,1)]
+
+# for Berg's particular stepper
+
+SEQ = [(1,0,0,1),
+       (1,0,0,0),
+       (1,1,0,0),
+       (0,1,0,0),
+       (0,1,1,0),
+       (0,0,1,0),
+       (0,0,1,1),
+       (0,0,0,1)]
+
+# define pins and initialize low
+GPIO.setmode(GPIO.BOARD)
+PINS = [13,15,16,18]
+GPIO.setup(PINS,
+           GPIO.OUT,
+           initial=GPIO.LOW)
+
+#
+# main line
+#
+
+if __name__ == '__main__':
+    # process id
+    print('\t= my pid:%d '% os.getpid())
+    print('\t======')
+
+    # names from controllers
+    #   names = ['stepX', 'stepY', 'stepZ']
+    names = ['stepX']
+
+    # list of controllers
+    controls = []   
+    # create a controller & its que.  add to our list of controls
+    for nam in names:
+            que = Queue()
+            mc = MotorController( nam, que, SEQ, PINS)
+            controls.append(mc)
+
+    # run some commands
+    for con in controls:
+        con.que.put( con.name) # send controller name. a NOOP cmd
+    time.sleep(1)
+
+    for con in controls:
+        con.que.put( 'step 10')  # do some steps
+    time.sleep(1)
+
+    # stop controller sub process
+    for con in controls:
+        con.que.put('quit')
+
+    
+    # time to quit
+    for c in controls:
+        c.proc.join()
+    GPIO.cleanup()
+    
+    
+
+
+
+
+
+
+
---
+++ stepper_controller.py
@@ -0,0 +1,78 @@
+""" stepper_controller.py
+
+Stepper motor controller for Raspberry Pi 
+"""
+
+from multiprocessing import Process, Queue
+import os
+import time
+import shlex
+import RPi.GPIO as GPIO
+
+
+class MotorController:
+    """R-Pi stepper controller.
+    language:
+    step N  - do N rotation steps
+    forward - set rotation direction
+    reverse - set rotation direction
+    quit    - kill subprocess
+    """
+
+    def __init__(self, name, msgque, seq, pins):
+        self.name  = name
+        self.que = msgque
+        self.seq = seq
+        self.next = 0
+        self.pins = pins
+        self.pulse_time = 0.01  # time pins are set high
+        self.proc = Process(target=self.run, args=())
+        self.proc.start()
+
+    def run(self):
+        runit = True
+        while(runit):
+            msg = shlex.split(self.que.get())
+            if msg:
+                print('msg:', msg)
+                if msg[0] == 'quit':
+                    break
+                if msg[0] == 'step':
+                    # step() wants a number, not a string
+                    self.step( int(msg[1]) )
+
+    def step(self, steps, wait_time=0.1):
+        """cycle motor steps number of steps"""
+        print("step %d" % steps)
+        # for each step, set the pins for the current pattern
+        for s in range(steps):
+            self.toggle_pins(self.pins,
+                             self.next_sequence())
+            time.sleep(wait_time)
+
+
+    def toggle_pins( self, pins, seq):
+        """ set pins according to sequence tuple """
+        print( seq)
+        # set pins for pulse time, then clear
+        for i in zip(pins, seq):
+            print(i)
+            GPIO.output(i[0], i[1])
+        time.sleep( self.pulse_time)
+        GPIO.output( i[0], GPIO.LOW)
+
+
+    def next_sequence(self):
+        """ returns next tuple of values from sequece """
+        seq = self.seq[self.next]
+        # bump pointer
+        # todo  handle reverse direction
+        self.next += 1
+        if self.next >= len(self.seq):
+            self.next = 0
+        return seq
+
+
+
+
+
