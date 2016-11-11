094939abe4fc1ff8630ae2248c735c33648058ba
Berg53 <mworlds@gmail.com>
2016-11-11 13:20:52 +1100

    adding menu

---
+++ menu_controller.py
@@ -0,0 +1,80 @@
+import tkinter as tk
+from motor_controller import StepperController
+from multiprocessing import Pipe
+from datetime import datetime
+import logging
+import sys
+import os
+con = None
+# for Berg's particular stepper
+SEQ = [(1, 0, 0, 1),
+       (1, 0, 0, 0),
+       (1, 1, 0, 0),
+       (0, 1, 0, 0),
+       (0, 1, 1, 0),
+       (0, 0, 1, 0),
+       (0, 0, 1, 1),
+       (0, 0, 0, 1)]
+
+#
+# define pins and initialize low
+#
+
+XPINS = [13, 15, 16, 18]
+YPINS = [37, 33, 31, 29]
+ZPINS = [32, 36, 38, 40]
+'''GPIO.setup(XPINS + YPINS + ZPINS,
+           GPIO.OUT,
+           initial=GPIO.LOW)'''
+
+class Hello:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
+        self.step1 = tk.Button(self.frame, text = 'step1', width = 25, command = self.step_controller('1'))
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.step1.pack()
+        self.quitButton.pack()
+        self.button1.pack()
+        self.frame.pack()
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Demo2(self.newWindow)
+    def close_windows(self):
+        self.master.destroy()
+    def step_controller(self, arg):
+        logger.debug("do_step() ")
+        global con
+        con.pipe.send('step 10')
+    
+
+class Demo2:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+    def close_windows(self):
+        self.master.destroy()
+
+def main():
+    global con
+     
+    root = tk.Tk()
+
+    # our process id
+    logger.info('main pid: %d ', os.getpid())
+            
+    con = StepperController.Factory('stepx', SEQ, XPINS)
+    app = Hello(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+            # set up logging
+    logging.basicConfig(filename='controller_menu.log',
+                            level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.info("Start: %s", datetime.now())
+    main()
--- reference_scripts/tkinter/async_tkinter.py
+++ reference_scripts/tkinter/async_tkinter.py
@@ -9,10 +9,8 @@ import os
 import sys
 from subprocess import Popen, PIPE, STDOUT
 
-
 try:
     import Tkinter as tk
-    from Tkinter import Canvas
 except ImportError: # Python 3
     import tkinter as tk
 
@@ -31,23 +29,6 @@ class ShowProcessOutputDemo:
     def __init__(self, root):
         """Start subprocess, make GUI widgets."""
         self.root = root
-
-        self.canvas = tk.Canvas(self, width=400, height=400)
-        self.canvas.pack(side="top", fill="both", expand=True)
-        self.canvas.create_line(200,200, 200,200, tags=("line",), arrow="last")
-        self.rotate()
-
-    def rotate(self, angle=0):
-        '''Animation loop to rotate the line by 10 degrees every 100 ms'''
-        a = math.radians(angle)
-        r = 50
-        x0, y0 = (200,200)
-        x1 = x0 + r*math.cos(a)
-        y1 = y0 + r*math.sin(a)
-        x2 = x0 + -r*math.cos(a)
-        y2 = y0 + -r*math.sin(a)
-        self.canvas.coords("line", x1,y1,x2,y2)
-        self.after(100, lambda angle=angle+10: self.rotate(angle))
 
         # start subprocess
         self.proc = Popen(cmd, stdout=PIPE, stderr=STDOUT)
