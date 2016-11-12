e9ac102e2befb0847773a3c57451845f15d5657e
Berg53 <mworlds@gmail.com>
2016-11-12 14:20:49 +1100

    adding directional image buttons and a few other text lables and a logo to be approved or not

--- menu_controller.py
+++ menu_controller.py
@@ -37,32 +37,92 @@ class Hello:
         self.odometerx = tk.StringVar()
         self.odometerx.set('odometer 0')
         self.Xodometer = tk.Label(self.frame, textvariable = self.odometerx)
+        self.odometery = tk.StringVar()
+        self.odometery.set('odometer 0')
+        self.Yodometer = tk.Label(self.frame, textvariable = self.odometery)
+        self.logo_image = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/python_logo.png')
+        self.logo = tk.Label(self.frame, image=self.logo_image)
+        self.left_image_up = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/leftup.png')
+        self.left_image_down = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/leftdown.png')
+        self.right_image_up = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/rightup.png')
+        self.right_image_down = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/rightdown.png')
+        
+        
+        self.up_image_up = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/upup.png')
+        self.up_image_down = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/leftdown.png')
+        self.down_image_up = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/downup.png')
+        self.down_image_down = tk.PhotoImage(file='/home/berg/bin/rpi-snippets/Images/downdown.png')
+        
         self.button1 = tk.Button(self.frame, text = 'New Window', width = 10, command = self.new_window)
-        self.use0 = tk.Button(self.frame, text = 'LEFT', width = 10, command = self.UseXController, repeatdelay = 500, repeatinterval = 1)
-        self.use1 = tk.Button(self.frame, text = 'UP', width = 10, command = self.UseYController, repeatdelay = 500, repeatinterval = 1)
+        
+
+        self.LEFT_BUTTON = tk.Button(self.frame, image=self.left_image_up, width = 50, command = self.leftXController, repeatdelay = 500, repeatinterval = 1)
+        self.LEFT_BUTTON.bind('<ButtonRelease-1>',self.Left_Motion_up())
+        self.LEFT_BUTTON.bind('<Button-1>',self.Left_Motion_down())
+
+        self.RIGHT_BUTTON = tk.Button(self.frame, image = self.right_image_up, width = 50, command = self.rightXController, repeatdelay = 500, repeatinterval = 1)
+        self.RIGHT_BUTTON.bind('<ButtonRelease-1>',self.Right_Motion_up())
+        self.RIGHT_BUTTON.bind('<Button-1>',self.Right_Motion_down())
+        
+        self.UP_BUTTON = tk.Button(self.frame, image=self.up_image_up, width = 50, command = self.upYController, repeatdelay = 500, repeatinterval = 1)        
+        self.UP_BUTTON.bind('<ButtonRelease-1>',self.Right_Motion_up())
+        self.UP_BUTTON.bind('<Button-1>',self.Right_Motion_down())
+        
+        self.DOWN_BUTTON = tk.Button(self.frame, image=self.down_image_up, width = 50, command = self.downYController, repeatdelay = 500, repeatinterval = 1)
+        self.DOWN_BUTTON.bind('<ButtonRelease-1>',self.Right_Motion_up())
+        self.DOWN_BUTTON.bind('<Button-1>',self.Right_Motion_down())
+        
         self.use2 = tk.Button(self.frame, text = 'usez', width = 10, command = self.UseZController, repeatdelay = 500, repeatinterval = 1)
-        self.stepRev0 = tk.Button(self.frame, text = 'RIGHT', width = 10, command = self.UseXRevController, repeatdelay = 500, repeatinterval = 1)
-        self.stepRev1 = tk.Button(self.frame, text = 'DOWN', width = 10, command = self.UseYRevController, repeatdelay = 500, repeatinterval = 1)
+
+
+        
         self.stepRev2 = tk.Button(self.frame, text = 'stepRevz', width = 10, command = self.UseZRevController, repeatdelay = 500, repeatinterval = 1)
 
-        self.odomX = tk.Button(self.frame, text = 'Odometer X', width = 10, command = self.OdomXController)
-        self.odomY = tk.Button(self.frame, text = 'Odometer Y', width = 10, command = self.OdomYController)
-        self.odomZ = tk.Button(self.frame, text = 'Odometer Z', width = 10, command = self.OdomZController)
+
 
         self.quitButton = tk.Button(self.frame, text = 'Quit', width = 10, command = self.close_windows)
-        self.Xodometer.grid(row = 0, column = 2)
-        self.odomX.grid(row=0,column=1)
-        self.odomY.grid(row=1,column=1)
-        self.odomZ.grid(row=2,column=1)
-        self.use0.grid(row=3,column=1)
-        self.use1.grid(row=4,column=1)
+        self.logo.grid(row = 1, column = 1, columnspan = 3)
+        self.Xodometer.grid(row = 0, column = 1)
+        self.Yodometer.grid(row = 0, column = 2)
+        
+        self.RIGHT_BUTTON.grid(row=3,column=6)
+        self.LEFT_BUTTON.grid(row=3,column=4)
+        
+        self.DOWN_BUTTON.grid(row=4,column=5)
+        self.UP_BUTTON.grid(row=2,column=5)
+        
         self.use2.grid(row=5,column=1)
-        self.stepRev0.grid(row=3,column=2)
-        self.stepRev1.grid(row=4,column=2)
         self.stepRev2.grid(row=5,column=2)
         self.quitButton.grid(row=9,column=1)
         self.button1.grid(row=10,column=1)
         self.frame.grid(row=11,column=1)
+#left and right button images        
+    def Left_Motion_up(self):
+        self.LEFT_BUTTON.config(image=self.left_image_up)
+        
+    def Left_Motion_down(self):
+        self.LEFT_BUTTON.config(image=self.left_image_down)
+        
+    def Right_Motion_up(self):
+        self.RIGHT_BUTTON.config(image=self.right_image_up)
+        
+    def Right_Motion_down(self):
+        self.RIGHT_BUTTON.config(image=self.right_image_down)
+        
+    def Up_Motion_up(self):
+        self.LEFT_BUTTON.config(image=self.up_image_up)
+        
+    def Up_Motion_down(self):
+        self.LEFT_BUTTON.config(image=self.up_image_down)
+        
+    def Down_Motion_up(self):
+        self.DOWN_BUTTON.config(image=self.down_image_up)
+        
+    def Down_Motion_down(self):
+        self.DOWN_BUTTON.config(image=self.down_image_down)
+        
+        
+        
     def new_window(self):
         print("new window")
         self.newWindow = tk.Toplevel(self.master)
@@ -70,21 +130,24 @@ class Hello:
 
 
     def close_windows(self):
-        master.destroy()
+        self.master.destroy()
+        os.system('killall python3')
 
-    def UseXController(self):
+    def leftXController(self):
         logger.debug("do_stepx() ")
-        con0.pipe.send('step 1')
+        con0.pipe.send('step -1')
         con0.pipe.send('get odometer 0')
         text = (con0.pipe.recv())
         text = text.strip('odometer: ')
         self.odometerx.set("Odometer: %s"%(text))
 
-    def UseYController(self):
+    def upYController(self):
         logger.debug("do_stepy() ")
         con1.pipe.send('step 1')
         con1.pipe.send('get odometer 1')
-        print(con1.pipe.recv())  
+        text = (con1.pipe.recv())
+        text = text.strip('odometer: ')
+        self.odometery.set("Odometery: %s"%(text))  
 
 
 
@@ -94,19 +157,21 @@ class Hello:
         con2.pipe.send('get odometer 2')
         print(con2.pipe.recv())
 
-    def UseXRevController(self):
+    def rightXController(self):
         logger.debug("do_stepx() ")
-        con0.pipe.send('step -1')
+        con0.pipe.send('step 1')
         con0.pipe.send('get odometer 0')
         text = (con0.pipe.recv())
         text = text.strip('odometer: ')
-        self.odometerx.set("Odometer: %s"%(text))
+        self.odometerx.set("Odometerx: %s"%(text))
 
-    def UseYRevController(self):
+    def downYController(self):
         logger.debug("do_stepy() ")
         con1.pipe.send('step -1')
         con1.pipe.send('get odometer 1')
-        print(con1.pipe.recv())  
+        text = (con1.pipe.recv())
+        text = text.strip('odometer: ')
+        self.odometery.set("Odometery: %s"%(text))  
 
 
     def UseZRevController(self):
