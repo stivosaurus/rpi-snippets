9a651c6d1b95b7c42299c2fe05d09859573f5de3
berg53 <mworlds@gmail.com>
2016-11-13 16:32:10 +1100

    fix bug

--- menu_controller.py
+++ menu_controller.py
@@ -42,7 +42,6 @@ class Hello:
 #screen controlls
 #------------------------------------------------------------#
         self.screen = tk.Canvas(self.frame, width = 600, height = 400, bg = 'green', cursor = 'dot', bd = 2)
-        self.master.rocus_force()
         self.screen.create_line(15, 25, 200, 25)
         self.screen.create_line(300, 35, 300, 200, dash=(4, 2))
         self.screen.create_line(55, 85, 155, 85, 105, 180, 55, 85)
